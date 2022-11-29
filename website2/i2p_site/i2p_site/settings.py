8e78b824acd90dfad436729345e0b2c04b55801a
YiChiCanCode <76959917+YiChiCanCode@users.noreply.github.com>
2022-11-29 17:48:00 -0600

    load static files

--- website2/i2p_site/i2p_site/settings.py
+++ website2/i2p_site/i2p_site/settings.py
@@ -116,6 +116,12 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+# define a set of places to look for static files
+"""STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    BASE_DIR / 'aboutus/static/aboutus/',
+]"""
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
